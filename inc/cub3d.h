5d91db04b9557b820f2f91dba5375f34d639def6
Lisa <lduheron@e3r8p4.42.fr>
2023-09-12 13:06:58 +0200

    Starting the project

---
+++ inc/cub3d.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lduheron <lduheron@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 13:04:21 by lduheron          #+#    #+#             */
+/*   Updated: 2023/09/12 13:05:42 by lduheron         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <unistd.h>
+
+# define ERROR_MALLOC -3
+# define ERROR_SYNTAX -2
+# define ERROR 0
+# define SUCCESS 1
+
+#endif
