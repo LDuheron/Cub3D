a2ec104f79f711ae250e76e7c6711ac81325c210
Clement BERNAZEAU <clem.bernazeau@orange.fr>
2023-09-25 16:37:25 +0200

    marche vraiment cette fois

--- srcs/window/events.c
+++ srcs/window/events.c
@@ -6,7 +6,7 @@
 /*   By: cbernaze <cbernaze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 12:13:39 by cbernaze          #+#    #+#             */
-/*   Updated: 2023/09/25 16:13:13 by cbernaze         ###   ########.fr       */
+/*   Updated: 2023/09/25 16:36:42 by cbernaze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,35 +31,35 @@ int	close_win_key(int keysym, t_graph *data)
 		ft_exit(data);
 	if (keysym == XK_w || keysym == XK_Up)
 	{
-		// if (data->map[(int)(data->ray.posX + data->ray.dirX * MOVE_SPEED)][(int)data->ray.posY] == 0)
-			data->ray.posX += data->ray.dirX * MOVE_SPEED;
-		// if (data->map[(int)data->ray.posX][(int)(data->ray.posY + data->ray.dirY * MOVE_SPEED)] == 0)
-			data->ray.posY += data->ray.dirY * MOVE_SPEED;
+		// if (data->map[(int)(data->ray.pos_x + data->ray.dir_x * MOVE_SPEED)][(int)data->ray.pos_y] == 0)
+			data->ray.pos_x += data->ray.dir_x * MOVE_SPEED;
+		// if (data->map[(int)data->ray.pos_x][(int)(data->ray.pos_y + data->ray.dir_y * MOVE_SPEED)] == 0)
+			data->ray.pos_y += data->ray.dir_y * MOVE_SPEED;
 	}
 	if (keysym == XK_s || keysym == XK_Down)
 	{
-		// if (data->map[(int)(data->ray.posX - data->ray.dirX * MOVE_SPEED)][(int)data->ray.posY] == 0)
-			data->ray.posX -= data->ray.dirX * MOVE_SPEED;
-		// if (data->map[(int)data->ray.posX][(int)(data->ray.posY - data->ray.dirY * MOVE_SPEED)] == 0)
-			data->ray.posY -= data->ray.dirY * MOVE_SPEED;
+		// if (data->map[(int)(data->ray.pos_x - data->ray.dir_x * MOVE_SPEED)][(int)data->ray.pos_y] == 0)
+			data->ray.pos_x -= data->ray.dir_x * MOVE_SPEED;
+		// if (data->map[(int)data->ray.pos_x][(int)(data->ray.pos_y - data->ray.dir_y * MOVE_SPEED)] == 0)
+			data->ray.pos_y -= data->ray.dir_y * MOVE_SPEED;
 	}
 	if (keysym == XK_a || keysym == XK_Left)
 	{
-		old_dir_x = data->ray.dirX;
-		data->ray.dirX = data->ray.dirX * cos(-ROT_SPEED) - data->ray.dirY * sin(-ROT_SPEED);
-		data->ray.dirY = old_dir_x * sin(-ROT_SPEED) + data->ray.dirY * cos(-ROT_SPEED);
-		old_plane_x = data->ray.planeX;
-		data->ray.planeX = data->ray.planeX * cos(-ROT_SPEED) - data->ray.planeY * sin(-ROT_SPEED);
-		data->ray.planeY = old_plane_x * sin(-ROT_SPEED) + data->ray.planeY * cos(-ROT_SPEED);
+		old_dir_x = data->ray.dir_x;
+		data->ray.dir_x = data->ray.dir_x * cos(-ROT_SPEED) - data->ray.dir_y * sin(-ROT_SPEED);
+		data->ray.dir_y = old_dir_x * sin(-ROT_SPEED) + data->ray.dir_y * cos(-ROT_SPEED);
+		old_plane_x = data->ray.plane_x;
+		data->ray.plane_x = data->ray.plane_x * cos(-ROT_SPEED) - data->ray.plane_y * sin(-ROT_SPEED);
+		data->ray.plane_y = old_plane_x * sin(-ROT_SPEED) + data->ray.plane_y * cos(-ROT_SPEED);
 	}
 	if (keysym == XK_d || keysym == XK_Right)
 	{
-		old_dir_x = data->ray.dirX;
-		data->ray.dirX = data->ray.dirX * cos(ROT_SPEED) - data->ray.dirY * sin(ROT_SPEED);
-		data->ray.dirY = old_dir_x * sin(ROT_SPEED) + data->ray.dirY * cos(ROT_SPEED);
-		old_plane_x = data->ray.planeX;
-		data->ray.planeX = data->ray.planeX * cos(ROT_SPEED) - data->ray.planeY * sin(ROT_SPEED);
-		data->ray.planeY = old_plane_x * sin(ROT_SPEED) + data->ray.planeY * cos(ROT_SPEED);
+		old_dir_x = data->ray.dir_x;
+		data->ray.dir_x = data->ray.dir_x * cos(ROT_SPEED) - data->ray.dir_y * sin(ROT_SPEED);
+		data->ray.dir_y = old_dir_x * sin(ROT_SPEED) + data->ray.dir_y * cos(ROT_SPEED);
+		old_plane_x = data->ray.plane_x;
+		data->ray.plane_x = data->ray.plane_x * cos(ROT_SPEED) - data->ray.plane_y * sin(ROT_SPEED);
+		data->ray.plane_y = old_plane_x * sin(ROT_SPEED) + data->ray.plane_y * cos(ROT_SPEED);
 	}
 	draw_rect(data);
 	ft_raycasting(data);
