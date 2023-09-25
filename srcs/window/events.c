e752adc2ad958cfbf45f059fb4bd4fb0ab5d5592
Clement BERNAZEAU <clem.bernazeau@orange.fr>
2023-09-25 17:27:49 +0200

    galere pour bouger sur cotes

--- srcs/window/events.c
+++ srcs/window/events.c
@@ -6,7 +6,7 @@
 /*   By: cbernaze <cbernaze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 12:13:39 by cbernaze          #+#    #+#             */
-/*   Updated: 2023/09/25 16:36:42 by cbernaze         ###   ########.fr       */
+/*   Updated: 2023/09/25 17:23:22 by cbernaze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,23 +43,41 @@ int	close_win_key(int keysym, t_graph *data)
 		// if (data->map[(int)data->ray.pos_x][(int)(data->ray.pos_y - data->ray.dir_y * MOVE_SPEED)] == 0)
 			data->ray.pos_y -= data->ray.dir_y * MOVE_SPEED;
 	}
-	if (keysym == XK_a || keysym == XK_Left)
+	if (keysym == XK_a)
 	{
-		old_dir_x = data->ray.dir_x;
-		data->ray.dir_x = data->ray.dir_x * cos(-ROT_SPEED) - data->ray.dir_y * sin(-ROT_SPEED);
-		data->ray.dir_y = old_dir_x * sin(-ROT_SPEED) + data->ray.dir_y * cos(-ROT_SPEED);
-		old_plane_x = data->ray.plane_x;
-		data->ray.plane_x = data->ray.plane_x * cos(-ROT_SPEED) - data->ray.plane_y * sin(-ROT_SPEED);
-		data->ray.plane_y = old_plane_x * sin(-ROT_SPEED) + data->ray.plane_y * cos(-ROT_SPEED);
+		data->ray.pos_x += MOVE_SPEED;
+		data->ray.pos_y += data->ray.dir_y * MOVE_SPEED;
 	}
-	if (keysym == XK_d || keysym == XK_Right)
+	if (keysym == XK_d)
+	{
+		data->ray.pos_x -= MOVE_SPEED;
+		data->ray.pos_y -= data->ray.dir_y * MOVE_SPEED;
+	}
+	if (keysym == XK_Left)
 	{
 		old_dir_x = data->ray.dir_x;
-		data->ray.dir_x = data->ray.dir_x * cos(ROT_SPEED) - data->ray.dir_y * sin(ROT_SPEED);
-		data->ray.dir_y = old_dir_x * sin(ROT_SPEED) + data->ray.dir_y * cos(ROT_SPEED);
+		data->ray.dir_x = data->ray.dir_x * cos(-ROT_SPEED)
+			- data->ray.dir_y * sin(-ROT_SPEED);
+		data->ray.dir_y = old_dir_x * sin(-ROT_SPEED)
+			+ data->ray.dir_y * cos(-ROT_SPEED);
 		old_plane_x = data->ray.plane_x;
-		data->ray.plane_x = data->ray.plane_x * cos(ROT_SPEED) - data->ray.plane_y * sin(ROT_SPEED);
-		data->ray.plane_y = old_plane_x * sin(ROT_SPEED) + data->ray.plane_y * cos(ROT_SPEED);
+		data->ray.plane_x = data->ray.plane_x * cos(-ROT_SPEED)
+			- data->ray.plane_y * sin(-ROT_SPEED);
+		data->ray.plane_y = old_plane_x * sin(-ROT_SPEED)
+			+ data->ray.plane_y * cos(-ROT_SPEED);
+	}
+	if (keysym == XK_Right)
+	{
+		old_dir_x = data->ray.dir_x;
+		data->ray.dir_x = data->ray.dir_x * cos(ROT_SPEED)
+			- data->ray.dir_y * sin(ROT_SPEED);
+		data->ray.dir_y = old_dir_x * sin(ROT_SPEED)
+			+ data->ray.dir_y * cos(ROT_SPEED);
+		old_plane_x = data->ray.plane_x;
+		data->ray.plane_x = data->ray.plane_x * cos(ROT_SPEED)
+			- data->ray.plane_y * sin(ROT_SPEED);
+		data->ray.plane_y = old_plane_x * sin(ROT_SPEED)
+			+ data->ray.plane_y * cos(ROT_SPEED);
 	}
 	draw_rect(data);
 	ft_raycasting(data);
